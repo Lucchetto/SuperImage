ae9f08d95e009f8d0b1d64d57a6c346d076539d3
Lucchetto <lucchetto.tie@live.com>
2023-01-05 22:14:24 +0100

    WIP
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- realesrgan/src/main/cpp/realesrgan.cpp
+++ realesrgan/src/main/cpp/realesrgan.cpp
@@ -13,6 +13,91 @@
 
 #include "realesrgan.h"
 
+float_ptr_array tile_to_float_array(const Eigen::Tensor<int, 2>& tile) {
+
+    // Convert input image RGB int array to float array
+    // with tensor shape [1, REALESRGAN_IMAGE_CHANNELS, tile height, tile width]
+    const int tile_size = tile.size();
+    const int* tile_data = tile.data();
+    const size_t tensor_size = tile_size * REALESRGAN_IMAGE_CHANNELS;
+    const auto float_buffer = (float *) malloc(sizeof(float) * tensor_size);
+    const int green_start_index = tile_size;
+    const int blue_start_index = tile_size * 2;
+    for (int i = 0; i < tile_size; i++) {
+        // Alpha is ignored
+        float_buffer[i] = (float)((tile_data[i] >> 16) & 0xff) / 255.0;
+        float_buffer[i + green_start_index] = (float)((tile_data[i] >> 8) & 0xff) / 255.0;
+        float_buffer[i + blue_start_index] = (float)((tile_data[i]) & 0xff) / 255.0;
+    }
+
+    return float_ptr_array {
+        .data = float_buffer,
+        .size = tensor_size,
+    };
+}
+
+int* process_tiles(
+        TfLiteInterpreter* interpreter,
+        const Eigen::TensorMap<Eigen::Tensor<int, 2>> image_tensor,
+        const int scale,
+        const int tile_size,
+        const int pre_padding) {
+
+    const int height = image_tensor.dimension(0);
+    const int width = image_tensor.dimension(1);
+    const int offset_step = tile_size - pre_padding;
+
+    for (int y = 0; y < height; y += offset_step) {
+        for (int x = 0; x < width; x += offset_step) {
+            const Eigen::array<Eigen::Index, 2> offsets = {y, x};
+
+            const bool final_row_tile = x + offset_step > width;
+            const bool final_column_tile = y + offset_step > height;
+
+            const Eigen::array<Eigen::Index, 2> extents = {
+                    final_column_tile ? (height - y) : tile_size,
+                    final_row_tile ? (width - x) : tile_size
+            };
+            const Eigen::Tensor<int, 2> tile = image_tensor.slice(offsets, extents);
+            const float_ptr_array model_input = tile_to_float_array(tile);
+
+            // Feed input into model
+            if (TfLiteTensorCopyFromBuffer(
+                    TfLiteInterpreterGetInputTensor(interpreter, 0),
+                    model_input.data,
+                    model_input.size * sizeof(float)) != kTfLiteOk) {
+                LOGE("Something went wrong when copying input buffer to input tensor");
+                free(model_input.data);
+                return nullptr;
+            }
+
+            // Run the interpreter
+            if (TfLiteInterpreterInvoke(interpreter) != kTfLiteOk) {
+                LOGE("Something went wrong when running the TFLite model");
+                free(model_input.data);
+                return nullptr;
+            }
+
+            free(model_input.data);
+
+            // Extract the output tensor data
+            const size_t output_tensor_pixels = tile.size() * pow(scale, 2);
+            const size_t output_tensor_size = output_tensor_pixels * REALESRGAN_IMAGE_CHANNELS;
+            float output_buffer[output_tensor_size];
+            if (TfLiteTensorCopyToBuffer(
+                    TfLiteInterpreterGetOutputTensor(interpreter, 0),
+                    output_buffer,
+                    output_tensor_size * sizeof(float)) != kTfLiteOk) {
+                LOGE("Something went wrong when copying output tensor to output buffer");
+                free(model_input.data);
+                return nullptr;
+            }
+
+            LOGI("Test");
+        }
+    }
+}
+
 const output_image_t* run_inference(
         const void* model_data,
         const long model_size,
@@ -59,11 +144,17 @@ const output_image_t* run_inference(
         TfLiteModelDelete(model);
         return nullptr;
     }
+
+    process_tiles(interpreter, input_image, scale, REALESRGAN_INPUT_TILE_SIZE, REALESRGAN_INPUT_TILE_PADDING);
+
     TfLiteTensor* model_input_tensor = TfLiteInterpreterGetInputTensor(interpreter, 0);
 
+    const Eigen::array<Eigen::Index, 2> offsets = {64, 64};
+    const Eigen::array<Eigen::Index, 2> extents = {64, 64};
+    const Eigen::Tensor<int, 2> cropped = input_image.slice(offsets, extents);
     // Treat tensor as an one-dimensional array for simplicity
-    const int input_image_size = input_image.size();
-    const int* input_image_data = input_image.data();
+    const int input_image_size = cropped.size();
+    const int* input_image_data = cropped.data();
 
     // Convert input image RGB int array to float array
     // with tensor shape [1, REALESRGAN_IMAGE_CHANNELS, REALESRGAN_INPUT_IMAGE_HEIGHT, REALESRGAN_INPUT_IMAGE_WIDTH]
@@ -102,8 +193,8 @@ const output_image_t* run_inference(
     // Extract the output tensor data
     const TfLiteTensor* output_tensor =
             TfLiteInterpreterGetOutputTensor(interpreter, 0);
-    const int output_tensor_pixels = input_image_size * pow(scale, 2);
-    const int output_tensor_size = output_tensor_pixels * REALESRGAN_IMAGE_CHANNELS;
+    const size_t output_tensor_pixels = input_image_size * pow(scale, 2);
+    const size_t output_tensor_size = output_tensor_pixels * REALESRGAN_IMAGE_CHANNELS;
     float output_buffer[output_tensor_size];
     status = TfLiteTensorCopyToBuffer(
             output_tensor, output_buffer,
--- realesrgan/src/main/cpp/realesrgan.h
+++ realesrgan/src/main/cpp/realesrgan.h
@@ -10,6 +10,8 @@
 #include "Eigen/CXX11/Tensor"
 
 #define REALESRGAN_IMAGE_CHANNELS 3
+#define REALESRGAN_INPUT_TILE_SIZE 64
+#define REALESRGAN_INPUT_TILE_PADDING 10
 
 #define LOG_TAG "RealESRGAN"
 #define LOGI(...) \
@@ -17,9 +19,18 @@
 #define LOGE(...) \
   ((void)__android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__))
 
+/**
+ * @param data the data stored in the array
+ * @param size the size of the array in floats
+ */
+struct float_ptr_array {
+    float* data;
+    size_t size;
+};
+
 struct output_image_t {
     int* data;
-    const int size;
+    const size_t size;
 };
 
 const output_image_t* run_inference(const void* model_data,
