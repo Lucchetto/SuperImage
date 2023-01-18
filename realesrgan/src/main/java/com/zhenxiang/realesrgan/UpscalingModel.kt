9e0ae1bb810438019c025f15feee1f435a48ef12
Lucchetto <lucchetto.tie@live.com>
2023-01-18 11:44:41 +0100

    Add x2 model support
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- realesrgan/src/main/java/com/zhenxiang/realesrgan/UpscalingModel.kt
+++ realesrgan/src/main/java/com/zhenxiang/realesrgan/UpscalingModel.kt
@@ -1,6 +1,7 @@
 package com.zhenxiang.realesrgan
 
 enum class UpscalingModel(val labelRes: Int, val assetPath: String, val scale: Int) {
+    X2_PLUS(R.string.x2_plus_model_label, "realesrgan-x2plus.mnn", 2),
     X4_PLUS(R.string.x4_plus_model_label, "realesrgan-x4plus.mnn", 4);
 
     companion object {
