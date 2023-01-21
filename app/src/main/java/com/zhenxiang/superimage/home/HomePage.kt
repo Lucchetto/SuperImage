698463811b02bcae231c95f02a079536b2d3b37f
Lucchetto <lucchetto.tie@live.com>
2023-01-21 21:22:19 +0100

    HomePage: prevent blur shadow from affecting layout size
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- app/src/main/java/com/zhenxiang/superimage/home/HomePage.kt
+++ app/src/main/java/com/zhenxiang/superimage/home/HomePage.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.core.net.toUri
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -115,13 +116,17 @@ private fun ImagePreview(
                 ) {
                     val blurShadow by blurShadowTransformation.blurBitmapFlow.collectAsStateWithLifecycle()
                     blurShadow?.let {
-                        AsyncImage(
-                            modifier = Modifier
-                                .requiredSize(it.width.toDp(), it.height.toDp()),
-                            model = ImageRequest.Builder(LocalContext.current).data(it).build(),
-                            alpha = 0.95f,
-                            contentDescription = null
-                        )
+                        Box(
+                            modifier = Modifier.requiredSize(0.dp)
+                        ) {
+                            AsyncImage(
+                                modifier = Modifier
+                                    .requiredSize(it.width.toDp(), it.height.toDp()),
+                                model = ImageRequest.Builder(LocalContext.current).data(it).build(),
+                                alpha = 0.95f,
+                                contentDescription = null
+                            )
+                        }
                     }
 
                     AsyncImage(
