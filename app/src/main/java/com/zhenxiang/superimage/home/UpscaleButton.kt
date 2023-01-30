523a2bc1a87abdb1ad2f91fc1194164f7180911c
Lucchetto <lucchetto.tie@live.com>
2023-01-30 09:23:17 +0100

    UpscaleButton: add previews for dialog
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- app/src/main/java/com/zhenxiang/superimage/home/UpscaleButton.kt
+++ app/src/main/java/com/zhenxiang/superimage/home/UpscaleButton.kt
@@ -2,7 +2,9 @@ package com.zhenxiang.superimage.home
 
 import android.Manifest
 import android.app.Activity
+import android.content.res.Configuration
 import android.os.Build
+import androidx.activity.compose.ManagedActivityResultLauncher
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.padding
@@ -14,6 +16,7 @@ import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import com.zhenxiang.superimage.R
@@ -21,6 +24,7 @@ import com.zhenxiang.superimage.ui.mono.MonoAlertDialog
 import com.zhenxiang.superimage.ui.mono.MonoButton
 import com.zhenxiang.superimage.ui.mono.MonoButtonIcon
 import com.zhenxiang.superimage.ui.mono.MonoCancelDialogButton
+import com.zhenxiang.superimage.ui.theme.MonoTheme
 import com.zhenxiang.superimage.ui.utils.RowSpacer
 import com.zhenxiang.superimage.utils.IntentUtils
 import com.zhenxiang.superimage.utils.writeStoragePermission
@@ -81,63 +85,96 @@ internal fun UpscaleButton(enabled: Boolean, onClick: () -> Unit) {
     }
 
     if (retryRequestPermission) {
-        /**
-         * Indicates whether we can retry requesting permission directly
-         */
-        val showPermissionRational = (context as Activity).shouldShowRequestPermissionRationale(
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-        )
-        MonoAlertDialog(
-            onDismissRequest = { retryRequestPermission = false },
-            title = { Text(stringResource(id = R.string.permission_denied_title)) },
-            content = {
-                Text(
-                    modifier = Modifier.padding(it),
-                    text = stringResource(
-                        id = if (showPermissionRational) {
-                            R.string.storage_permission_denied_desc
-                        } else {
-                            R.string.storage_permission_denied_forever_desc
-                        }
-                    )
-                )
-            },
-            buttons = {
-                MonoCancelDialogButton{ retryRequestPermission = false }
-                RowSpacer()
-                if (showPermissionRational) {
-                    MonoButton(
-                        onClick = {
-                            /**
-                             * Dismiss dialog and request permission again
-                             */
-                            retryRequestPermission = false
-                            permissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                        }
-                    ) {
-                        Text(
-                            stringResource(id = R.string.grant_permission_label),
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis
-                        )
-                    }
-                } else {
-                    MonoButton(
-                        onClick = {
-                            /**
-                             * Redirect user to app settings page hoping they can figure it out
-                             */
-                            context.startActivity(IntentUtils.appSettingsIntent(context))
-                        }
-                    ) {
-                        MonoButtonIcon(
-                            painter = painterResource(id = R.drawable.ic_gear_24),
-                            contentDescription = null
-                        )
-                        Text(stringResource(id = R.string.open_settings_label))
-                    }
-                }
-            }
-        )
+        PermissionRequestDialog(
+            showPermissionRationale = (context as Activity).shouldShowRequestPermissionRationale(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ),
+            permissionLauncher = permissionLauncher
+        ) {
+            retryRequestPermission = false
+        }
     }
+}
+
+/**
+ * @param showPermissionRationale Indicates whether we can retry requesting permission directly
+ */
+@Composable
+private fun PermissionRequestDialog(
+    showPermissionRationale: Boolean,
+    permissionLauncher: ManagedActivityResultLauncher<String, Boolean>,
+    onDismissRequest: () -> Unit
+) = MonoAlertDialog(
+    onDismissRequest = onDismissRequest,
+    title = { Text(stringResource(id = R.string.permission_denied_title)) },
+    content = {
+        Text(
+            modifier = Modifier.padding(it),
+            text = stringResource(
+                id = if (showPermissionRationale) {
+                    R.string.storage_permission_denied_desc
+                } else {
+                    R.string.storage_permission_denied_forever_desc
+                }
+            )
+        )
+    },
+    buttons = {
+        MonoCancelDialogButton(onDismissRequest)
+        RowSpacer()
+        if (showPermissionRationale) {
+            MonoButton(
+                onClick = {
+                    /**
+                     * Dismiss dialog and request permission again
+                     */
+                    onDismissRequest()
+                    permissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            ) {
+                Text(
+                    stringResource(id = R.string.grant_permission_label),
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+        } else {
+            val context = LocalContext.current
+            MonoButton(
+                onClick = {
+                    /**
+                     * Redirect user to app settings page hoping they can figure it out
+                     */
+                    context.startActivity(IntentUtils.appSettingsIntent(context))
+                }
+            ) {
+                MonoButtonIcon(
+                    painter = painterResource(id = R.drawable.ic_gear_24),
+                    contentDescription = null
+                )
+                Text(stringResource(id = R.string.open_settings_label))
+            }
+        }
+    }
+)
+
+
+@Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun PermissionRequestDialogRationalePreview() = MonoTheme {
+    PermissionRequestDialog(
+        showPermissionRationale = true,
+        permissionLauncher = rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) {}
+    ) { }
+}
+
+@Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun PermissionRequestDialogPreview() = MonoTheme {
+    PermissionRequestDialog(
+        showPermissionRationale = false,
+        permissionLauncher = rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) {}
+    ) { }
 }
