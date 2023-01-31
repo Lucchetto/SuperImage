4d39dd2dc1285e32310ce3106e63a8351cabb2cb
Lucchetto <lucchetto.tie@live.com>
2023-01-31 16:20:09 +0100

    Add splash screen
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- app/src/main/java/com/zhenxiang/superimage/MainActivity.kt
+++ app/src/main/java/com/zhenxiang/superimage/MainActivity.kt
@@ -3,6 +3,7 @@ package com.zhenxiang.superimage
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.zhenxiang.superimage.home.HomePage
 import com.zhenxiang.superimage.ui.theme.MonoTheme
@@ -10,6 +11,8 @@ import com.zhenxiang.superimage.ui.theme.MonoTheme
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Handle the splash screen transition.
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             MonoTheme {
