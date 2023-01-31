e4db70c04c0faef13aa090482b292c83c30c00b4
Lucchetto <lucchetto.tie@live.com>
2023-01-31 23:18:39 +0100

    Support x4plus anime model
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- realesrgan/src/main/java/com/zhenxiang/realesrgan/UpscalingModel.kt
+++ realesrgan/src/main/java/com/zhenxiang/realesrgan/UpscalingModel.kt
@@ -2,7 +2,8 @@ package com.zhenxiang.realesrgan
 
 enum class UpscalingModel(val labelRes: Int, val assetPath: String, val scale: Int) {
     X2_PLUS(R.string.x2_plus_model_label, "realesrgan-x2plus.mnn", 2),
-    X4_PLUS(R.string.x4_plus_model_label, "realesrgan-x4plus.mnn", 4);
+    X4_PLUS(R.string.x4_plus_model_label, "realesrgan-x4plus.mnn", 4),
+    X4_PLUS_ANIME(R.string.x4_plus_anime_model_label, "realesrgan-x4plus-anime.mnn", 4);
 
     companion object {
         val VALUES = values()
