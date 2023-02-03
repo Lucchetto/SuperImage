30f73b7f16df82abf22c1edf305c0c2874e102f6
Lucchetto <lucchetto.tie@live.com>
2023-02-03 22:44:44 +0100

    Add compose navigation workaround for Xiaomi
        * On some Xiaomi devices the screen will stay on
        * a blank screen, add a dirty workaround to solve it
        * https://issuetracker.google.com/issues/227926002
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- app/src/main/java/com/zhenxiang/superimage/MainActivity.kt
+++ app/src/main/java/com/zhenxiang/superimage/MainActivity.kt
@@ -4,21 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScaffoldDefaults
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
+import androidx.navigation.compose.NavHost
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.zhenxiang.superimage.navigation.RootNavigation
 import com.zhenxiang.superimage.ui.theme.MonoTheme
 
 class MainActivity : ComponentActivity() {
 
-    @OptIn(ExperimentalAnimationApi::class)
+    @OptIn(ExperimentalAnimationApi::class, ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         // Handle the splash screen transition.
         installSplashScreen()
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
         setContent {
+            /**
+             * Due to this bug https://issuetracker.google.com/issues/227926002 when using
+             * splash screen API on some devices with Compose Jetpack Navigation leads to a blank
+             * screen. However wrapping the [NavHost] in a [Scaffold] solves this issue, so after
+             * digging a bit, I figured out the following line is the workaround for the
+             * blank screen bug
+             */
+            ScaffoldDefaults.contentWindowInsets
             MonoTheme {
                 RootNavigation(rememberAnimatedNavController())
             }
