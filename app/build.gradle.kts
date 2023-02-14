e7ac129b6c9722def53c923707b197568967e932
Lucchetto <lucchetto.tie@live.com>
2023-02-14 14:39:56 +0100

    Remove unnecessary compose options
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,12 +73,6 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.0"
-    }
     packagingOptions {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
