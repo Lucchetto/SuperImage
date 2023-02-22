f2fce1800f119af23defef4b64f6f1e7f82dce5f
Timothy Russo <russotimothy@live.it>
2023-02-22 20:59:47 +0100

    fix: change the suffix of the upscaling model

--- realesrgan/src/main/java/com/zhenxiang/realesrgan/UpscalingModel.kt
+++ realesrgan/src/main/java/com/zhenxiang/realesrgan/UpscalingModel.kt
@@ -11,7 +11,7 @@ enum class UpscalingModel(
 ): Identifiable<Int> {
     X2_PLUS(0, R.string.x2_plus_model_label, "realesrgan-x2plus.mnn", 2, "_x2"),
     X4_PLUS(1, R.string.x4_plus_model_label, "realesrgan-x4plus.mnn", 4, "_x4"),
-    X4_PLUS_ANIME(2, R.string.x4_plus_anime_model_label, "realesrgan-x4plus-anime.mnn", 4, "_x4_drawing");
+    X4_PLUS_ANIME(2, R.string.x4_plus_anime_model_label, "realesrgan-x4plus-anime.mnn", 4, "_x4 _x8 _x4_drawing");
 
     companion object: Identifiable.EnumCompanion<UpscalingModel> {
         override val VALUES = values()
