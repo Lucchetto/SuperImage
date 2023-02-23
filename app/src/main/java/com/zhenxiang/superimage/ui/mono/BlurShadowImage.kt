4a7ae3f4f515381a4e0b639b5711112a22af4c72
Lucchetto <lucchetto.tie@live.com>
2023-02-23 09:23:47 +0100

    BlurShadowImage: refactor layout measure
        * Use SubcomposeLayout to measure image first
        * Then use (image size + blur radius) as blur bitamp size
        * This fixes blur background being too big when layout resizes (like rotating to landscape)
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- app/src/main/java/com/zhenxiang/superimage/ui/mono/BlurShadowImage.kt
+++ app/src/main/java/com/zhenxiang/superimage/ui/mono/BlurShadowImage.kt
@@ -2,21 +2,22 @@ package com.zhenxiang.superimage.ui.mono
 
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.SubcomposeLayout
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
-import coil.compose.*
+import coil.compose.AsyncImage
 import coil.request.CachePolicy
 import coil.request.ImageRequest
 import coil.transition.CrossfadeTransition
-import com.zhenxiang.superimage.ui.dpToPx
-import com.zhenxiang.superimage.ui.pxToDp
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
+
+private val BLUR_RADIUS = 70.dp
 
 @Composable
 fun BlurShadowImage(
@@ -24,51 +25,58 @@ fun BlurShadowImage(
     contentDescription: String?,
     modifier: Modifier = Modifier,
     imageModifier: Modifier = Modifier,
-) = Box(
-    modifier = modifier,
-    contentAlignment = Alignment.Center
 ) {
     val context = LocalContext.current
     val coroutineScope = rememberCoroutineScope()
     val blurShadowProvider = remember { BlurShadowProvider(context) }
     var blurShadowBitmap by remember { mutableStateOf<Bitmap?>(null) }
-    val blurRadius = 70.dp.dpToPx()
-
-    SubcomposeAsyncImage(
-        model = model,
-        contentDescription = contentDescription,
-        success = {
-            BlurShadow(blurShadowBitmap)
-            SubcomposeAsyncImageContent(modifier = imageModifier)
-        },
-        onLoading = { blurShadowBitmap = null },
-        onSuccess = {
-            coroutineScope.launch(Dispatchers.IO) {
-                (it.result.drawable as? BitmapDrawable)?.bitmap?.let { bitmap ->
-                    blurShadowProvider.getBlurShadow(bitmap, blurRadius)?.let { blur ->
-                        blurShadowBitmap = blur
-                    }
-                }
-            }
-        }
-    )
-}
-
-@Composable
-private fun SubcomposeAsyncImageScope.BlurShadow(blurBitmap: Bitmap?) = blurBitmap?.let {
-
     val crossfade = remember { CrossfadeTransition.Factory(1000) }
 
-    Box(modifier = Modifier.requiredSize(0.dp)) {
-        this@BlurShadow.SubcomposeAsyncImageContent(
-            modifier = Modifier.requiredSize(it.width.pxToDp(), it.height.pxToDp()),
-            painter = rememberAsyncImagePainter(
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data(it)
-                    .transitionFactory(crossfade)
-                    .memoryCachePolicy(CachePolicy.DISABLED)
-                    .build()
+    SubcomposeLayout(modifier) { constraints ->
+
+        val blurRadius = BLUR_RADIUS.value * density
+        val blurRadiusInt = blurRadius.roundToInt()
+
+        val imagePlaceable = subcompose(0) {
+            AsyncImage(
+                modifier = imageModifier,
+                model = model,
+                contentDescription = contentDescription,
+                onLoading = { blurShadowBitmap = null },
+                onSuccess = {
+                    coroutineScope.launch(Dispatchers.IO) {
+                        (it.result.drawable as? BitmapDrawable)?.bitmap?.let { bitmap ->
+                            blurShadowProvider.getBlurShadow(bitmap, blurRadius)?.let { blur ->
+                                blurShadowBitmap = blur
+                            }
+                        }
+                    }
+                }
             )
-        )
+        }[0].measure(constraints)
+
+        val blurPlaceable = blurShadowBitmap?.let {
+            subcompose(1) {
+                AsyncImage(
+                    modifier = Modifier.fillMaxSize(),
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(it)
+                        .transitionFactory(crossfade)
+                        .memoryCachePolicy(CachePolicy.DISABLED)
+                        .build(),
+                    contentDescription = null
+                )
+            }[0].measure(
+                Constraints.fixed(
+                    imagePlaceable.width + blurRadiusInt * 2,
+                    imagePlaceable.height + blurRadiusInt * 2
+                )
+            )
+        }
+
+        layout(imagePlaceable.width, imagePlaceable.height) {
+            blurPlaceable?.place(blurRadiusInt * -1, blurRadiusInt * -1)
+            imagePlaceable.place(0, 0)
+        }
     }
 }
