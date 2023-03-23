796c6a4c8f7b278949edf2b5d1a73cc11b7cfc6b
Lucchetto <lucchetto.tie@live.com>
2023-03-23 22:59:54 +0100

    Version 1.3.1
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.zhenxiang.superimage"
         minSdk = 24
         targetSdk = 33
-        versionCode = 130
-        versionName = "1.3.0"
+        versionCode = 131
+        versionName = "1.3.1"
 
         buildConfigField("String", "CHANGELOG_ASSET_NAME", "\"$changelogFileName\"")
 
